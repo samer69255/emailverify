19a890f0f6bd13d9b952ffe21747be9449813c18
sama <sam@sa.com>
2020-09-28 12:31:20 +0000

    2020

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var fs = require("fs");
+var emailCheck = require("email-check");
+
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  var pass = "1223455";
+	if (req.query.email) {
+    if (req.query.pass !== pass) return res.end('access deny');
+    var r = await verifyEmail(req.query.email);
+		res.end(JSON.stringify({verify: r}));
+		return;
+	}
+  res.render('index', { title: 'Express' });
+});
+
+
+function verifyEmail(email) {
+  return new Promise(resolve => {
+
+    emailCheck(email)
+  .then(function (res) {
+    // Returns "true" if the email address exists, "false" if it doesn't.
+    resolve(res);
+  })
+  .catch(function (err) {
+    console.log('error: ', err);
+    if (err.message === 'refuse') {
+      // The MX server is refusing requests from your IP address.
+      resolve('un');
+      console.log('err1')
+    } else {
+      // Decide what to do with other errors.
+      console.log('err2');
+      resolve('un');
+    }
+  });
+
+  });
+}
+
+(async function () {
+  console.log('testing');
+  console.log(await verifyEmail('samer954955@gmail.com'));
+  console.log(await verifyEmail('samer6925055@yahoo.com'));
+})();
+
+
+
+
+module.exports = router;
